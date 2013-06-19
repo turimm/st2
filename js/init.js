aaf6562309e58d9cc4bcfad960aded1198fc7490
mohamed <mohor@live.ru>
2013-06-19 16:04:43 +0300

    car_type

--- js/init.js
+++ js/init.js
@@ -830,7 +830,7 @@ function set_profile(){
                         break;
                 }
             });
-            $button_user.closest("form").find("select option[value=" + localStorage.getItem("car_type") + "]").attr("checked", "checked");
+            $button_user.closest("form").find("select option[value=" + localStorage.getItem("car_type") + "]").attr("seleced", "seleced");
         }
     }
 }
