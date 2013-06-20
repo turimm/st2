7cdaa35cc4242a662e7073dc19a70f6d71f27c1a
Sergey LEvchenko <sdsd@example.com>
2013-06-20 16:36:43 +0300

    add animation

--- js/init.js
+++ js/init.js
@@ -1472,6 +1472,15 @@ $(document).ready(function(){
             return false;
         });*/
 
+//    animated for tachometer
 
+    function addAngle( elem_ev, angle){
+        $(elem_ev).addClass(angle);
+    }
+    $(".js_event").click(function(){
+        var select_el = $(".sl_arrow_tachometer");
+        var add = $(".rotate_180");
+        addAngle(select_el, add);
+    });
 });
 
